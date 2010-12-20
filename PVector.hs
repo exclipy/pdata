4e6e279ce889238fedd4f51da295ab1cf6c03fad
Kevin Wu Won <exclipy@gmail.com>
2010-12-20 23:26:38 +1000

    Finish support for unlimited append

--- PVector.hs
+++ PVector.hs
@@ -1,10 +1,14 @@
-module PVector () where
+module PVector (main) where
 
-import PVector.Internal as I
+import Control.Monad
 import Data.Array
 import Data.Bits hiding (shift)
 import Control.Exception
 import Prelude hiding (tail)
+
+shiftStep = 5
+chunk = 2^shiftStep
+mask = pred chunk
 
 data PVector e = PV {
       cnt :: Int
@@ -14,66 +18,71 @@ data PVector e = PV {
     }
 
 instance (Show e) => Show (PVector e) where
-    show pv = show $ map ((tail pv) !) [0..(cnt pv)-1]
+    show (PV c s r t) = "c = " ++ (show c) ++
+                        ", s = " ++ (show s) ++
+                        ", r = " ++ (show r) ++
+                        ", t: " ++ show (elems t)
 
 data Node e = BodyNode (Array Int (Node e)) |
-              TailNode (Array Int e)
+              LeafNode (Array Int e)
+
+instance (Show e) => Show (Node e) where
+    show (BodyNode x) = show $ elems x
+    show (LeafNode x) = show $ elems x
 
 emptyNode :: Node e
-emptyNode = Node $ array (0, 31) []
+emptyNode = BodyNode $ array (0, -1) []
 
 empty :: PVector e
-empty = PV 0 5 emptyNode $ array (0, -1) []
+empty = PV 0 shiftStep emptyNode $ array (0, -1) []
 
 index :: PVector e -> Int -> e
 pv `index` ix | ix >= cnt pv = throw $ IndexOutOfBounds ""
-              | ix >= 32     = throw $ IndexOutOfBounds "TODO"
+              | ix >= chunk  = throw $ IndexOutOfBounds "TODO"
               | otherwise    = (tail pv) ! ix
 
 (|>) :: PVector e -> e -> PVector e
-pv |> el =
-    let c = cnt pv
-        s = shift pv
-        r = root pv
-        t = tail pv
-        tailIx = c - tailoff c
-        in if tailIx < 32
+(PV c s r t) |> el =
+    let tailIx = c - tailoff c
+        in if tailIx < chunk
               then let newTail = listArray (0, tailIx) $ elems t ++ [el]
                        in PV (c+1) s r newTail
-              else let overflow = ((c `shiftR` s) > (1 `shiftL` s))
+              else let overflow = ((c `shiftR` shiftStep) > (1 `shiftL` s))
                        newShift = if overflow
-                                     then s
-                                     else s + 5
+                                     then s + shiftStep
+                                     else s
                        newRoot = if overflow
-                                    then undefined
+                                    then BodyNode $ listArray (0, 1) [r, newPath s t]
                                     else pushTail c s r t
                        newTail = listArray (0, 0) [el]
                        in PV (c+1) newShift newRoot newTail
 
-    where pushTail :: Int -> Int -> Node e -> Array Int e -> Node e
-          pushTail cnt level (Node parent) tail =
-              let subIx = ((cnt-1) `shiftR` level) .&. 0x1f
-                  in if level == 5
-                        then Node $ parent // [(subIx, tail)]
-                        else undefined
+pushTail :: Int -> Int -> Node e -> Array Int e -> Node e
+pushTail cnt level (BodyNode parent) tail =
+    let subIx = ((cnt-1) `shiftR` level) .&. mask
+        in if level == shiftStep
+              then BodyNode $ listArray (0, subIx) $ elems parent ++ [LeafNode tail]
+              else if subIx > snd (bounds parent)
+                      then BodyNode (listArray (0, subIx) $ elems parent ++
+                                     [newPath (level-shiftStep) tail])
+                      else BodyNode (parent //
+                              [(subIx, pushTail cnt (level-shiftStep) (parent ! subIx) tail)])
+
+newPath :: Int -> Array Int e -> Node e
+newPath 0 t = LeafNode t
+newPath s t = BodyNode $ listArray (0, 0) [newPath (s-shiftStep) t]
 
 
 update :: Int -> e -> PVector e -> PVector e
 update ix el pv | ix >= cnt pv = throw $ IndexOutOfBounds ""
-                | ix >= 32     = throw $ IndexOutOfBounds "TODO"
+                | ix >= chunk  = throw $ IndexOutOfBounds "TODO"
                 | otherwise    = PV (cnt pv) (shift pv) (root pv) (tail pv // [(ix, el)])
 
 tailoff :: Int -> Int
-tailoff count = if count < 32
+tailoff count = if count < chunk
                    then 0
-                   else (count-1) .&. (complement 0x1f)
+                   else (count-1) .&. (complement mask)
 
 main = do
-    let a = (empty :: PVector Int)
-        b = a |> 1
-        c = foldl (|>) empty [1..32]
-        d = foldl (|>) empty [1..33]
-        in do print a
-              print b
-              print c
-              print d
+    forM_ (map (\x -> foldl (|>) empty [1..x]) [1..40])
+          (\pv -> print pv)
